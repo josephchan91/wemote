b17a7f9d9a2d7bf23203445b935ee758c3af5898
Joseph Chan <josephchan91@gmail.com>
2013-09-29 15:02:10 -0400

    trying to make ytplayer work

--- app/assets/javascripts/youtube_player.js
+++ app/assets/javascripts/youtube_player.js
@@ -3,6 +3,7 @@ function loadYouTubePlayer() {
   var atts = { id: "myytplayer" };
   // change this url later, it's pointing to a kanye west video
   var url = "http://www.youtube.com/v/x36nVPJWVdk?enablejsapi=1&playerapiid=ytplayer&version=3";
+  $('body').css('background-color', 'red');
   swfobject.embedSWF(
     url,
     "ytapiplayer",
@@ -14,7 +15,6 @@ function loadYouTubePlayer() {
     params,
     atts
   );
-  $('body').css('background-color', 'red');
 }
 
 function onYouTubePlayerReady(playerId) {
