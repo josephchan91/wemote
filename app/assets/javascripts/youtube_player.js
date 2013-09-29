ccbe39b562d710a754c9bb0354102a1fb2ce863c
Joseph Chan <josephchan91@gmail.com>
2013-09-29 14:31:56 -0400

    add ytplayer

---
+++ app/assets/javascripts/youtube_player.js
@@ -0,0 +1,21 @@
+function loadYouTubePlayer() {
+  var params = { allowScriptAccess: "always" };
+  var atts = { id: "myytplayer" };
+  // change this url later, it's pointing to a kanye west video
+  var url = "http://www.youtube.com/v/x36nVPJWVdk?enablejsapi=1&playerapiid=ytplayer&version=3";
+  swfobject.embedSWF(
+    url,
+    "ytapiplayer",
+    "425",
+    "356",
+    "8",
+    null,
+    null,
+    params,
+    atts
+  );
+}
+
+function onYouTubePlayerReady(playerId) {
+  ytplayer = document.getElementById("myytplayer");
+}
